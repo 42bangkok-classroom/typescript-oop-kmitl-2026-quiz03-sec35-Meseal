648c9b40a3b242b1c4104f045c4e8e1f84ffc9dc
68011128_Sahapoom Rattanawiwatpong <68011128@kmitl.ac.th>
2026-02-16 09:56:56 +0700

    p02test1andp01full

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,18 @@
-export class Person {}
+export class Person {
+    public firstname: string = '';
+    public lastname: string = '';
+    private age: number = 0;
+    public static COUNTRY: string = 'ไทย';
+    
+    public getFullName(): string {
+        return(this.firstname + " " + this.lastname)
+    }
+    setAge(age: number)
+    {
+        this.age = age
+    }
+    getAge(): number 
+    {
+        return this.age
+    }
+}
